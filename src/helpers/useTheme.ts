9ce1c4ea95e245a35b2a80ce19b86e67fc320647
lucasellery <olucasellery@gmail.com>
2022-08-31 23:18:15 -0300

    first commit

---
+++ src/helpers/useTheme.ts
@@ -0,0 +1,14 @@
+import { ThemeOptions } from '@mui/material/styles/createTheme';
+import { PaletteOptions } from '@mui/material';
+
+export const useTheme: ThemeOptions = {
+  palette: {
+    primary: {
+      main: '#3f51b5',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+  },
+  
+};
